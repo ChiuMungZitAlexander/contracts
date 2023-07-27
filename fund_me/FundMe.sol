83719b88bd358dd42b5d9b6961aa0cb77f308c3e
AlexanderZhao <zhaomengzhe@gmail.com>
2023-07-27 19:36:33 +0800

    feat(): FundMe

---
+++ fund_me/FundMe.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT License
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract FundMe {
+    uint256 public minimumUsd = 50 * 1e18;
+
+    function fund() public payable {
+        require(
+            getConversionRate(msg.value) > minimumUsd,
+            "Didn't send enought"
+        );
+    }
+
+    function getPrice() public view returns (uint256) {
+        // ABI
+        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+
+        return uint256(price * 1e10);
+    }
+
+    function getVersion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+        return priceFeed.version();
+    }
+
+    function getConversionRate(
+        uint256 ethAmount
+    ) public view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+
+    // function widthdraw() {}
+}
